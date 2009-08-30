3f76190c64c3674a26a1d3045a764c0373d83b41
Ramon Wijnands <ramon.wijnands007@gmail.com>
2009-08-30 18:36:25 +0200

    Added 'updateserverinfo' project

---
+++ updateserverinfo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace updateserverinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+
+        List<string> files = new List<string>();
+
+        public Program()
+        {
+            Console.WriteLine("Searching for games...");
+            DirectoryInfo dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            SearchRecursive(dir, "./");
+
+            //Write to file
+            TextWriter tw = new StreamWriter("server.info.txt");
+
+            //This is so the manager can recognise it
+            tw.Write("lol.dirlist\0");
+
+            //Add all entries
+            foreach (string file in this.files)
+                tw.Write(file + '\0');
+
+            //Close the reader
+            tw.Close();
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        public void SearchRecursive(DirectoryInfo dir, string path)
+        {
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (file.Name == "lol.info.xml")
+                {
+                    string fileName = path + file.Name;
+                    this.files.Add(fileName);
+                    Console.WriteLine(fileName);
+                }
+            }
+
+            foreach (DirectoryInfo child in dir.GetDirectories())
+            {
+                SearchRecursive(child, path + child.Name + "/");
+            }
+        }
+    }
+}
