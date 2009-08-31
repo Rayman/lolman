df4bbfd2d40cb89f60bd4ab6d67684248ddc9edd
Ramon Wijnands <ramon.wijnands007@gmail.com>
2009-08-31 20:32:24 +0200

    Lolgen2 Finished

--- lolgen2/Form1.Designer.cs
+++ lolgen2/Form1.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            //
+            // buttonSelectFolder
+            //
+            this.buttonSelectFolder.Location = new System.Drawing.Point(13, 13);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(93, 23);
+            this.buttonSelectFolder.TabIndex = 0;
+            this.buttonSelectFolder.Text = "Select Folder";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            //
+            // textBoxFolder
+            //
+            this.textBoxFolder.Location = new System.Drawing.Point(113, 13);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(371, 20);
+            this.textBoxFolder.TabIndex = 1;
+            //
+            // labelStatus
+            //
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(13, 43);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "label1";
+            //
+            // progressBar1
+            //
+            this.progressBar1.Location = new System.Drawing.Point(16, 60);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(535, 23);
+            this.progressBar1.TabIndex = 3;
+            //
+            // buttonStart
+            //
+            this.buttonStart.Location = new System.Drawing.Point(490, 13);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(61, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             //
             // Form1
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(431, 275);
+            this.ClientSize = new System.Drawing.Size(563, 188);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.MaximumSize = new System.Drawing.Size(579, 224);
+            this.MinimumSize = new System.Drawing.Size(579, 224);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- lolgen2/Form1.cs
+++ lolgen2/Form1.cs
@@ -6,14 +6,56 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace LanOfLegends.lolgen2
 {
     public partial class Form1 : Form
     {
+        private DirectorySummer backgroundWorker;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Select the folder of the game";
+            dialog.ShowNewFolderButton = false;
+            dialog.Tag = "asdf";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string folder = (new DirectoryInfo(dialog.SelectedPath)).FullName;
+                textBoxFolder.Text = folder;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folder = (new DirectoryInfo(textBoxFolder.Text + Path.DirectorySeparatorChar)).ToString();
+                backgroundWorker = new DirectorySummer(folder, "asdf");
+                backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(summerProgressChanged);
+                backgroundWorker.RunWorkerCompleted += delegate(object send, RunWorkerCompletedEventArgs args)
+                {
+                    MessageBox.Show("Done!");
+                };
+                backgroundWorker.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void summerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+            this.labelStatus.Text = e.UserState as string;
         }
     }
 }
